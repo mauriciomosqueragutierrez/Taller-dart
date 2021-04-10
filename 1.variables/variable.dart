549caacd541833464a9a78a82675fc10203b3849
mauriciomosqueragutierrez <mauricio.mosquera.gutierrez@gmail.com>
2021-04-10 18:12:58 -0500

    Creacion variables

---
+++ 1.variables/variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nombre = "Mauricio";
+  int edad = 25;
+  String telefono = "3165543748";
+  int? variabeAceptaNull = null;
+  final String segundoApellido = "Gutierrez";
+
+  var apellido;
+  print(nombre);
+  print(apellido);
+}
