3ea378c2fd23f478f4165ed88c93e54ff4e7c0c0
mauriciomosqueragutierrez <mauricio.mosquera.gutierrez@gmail.com>
2021-04-10 18:55:53 -0500

    ajuste traslations

--- 2.tipos/tipos.dart
+++ 2.tipos/tipos.dart
@@ -43,7 +43,7 @@ void main() {
   //var texto10 = r"Se interpola \ '$texto8 \' con el valor de la variable número 1 = $numero1";
   print(texto8);
   print(texto9);
- var texto10 = "It \n s easy to escape the string delimiter.";
+  var texto10 = "It \n s easy to escape the string delimiter.";
   print(texto10);
 
   // multi linea
@@ -62,47 +62,45 @@ void main() {
   var texto12 = '';
   if (texto12.isEmpty) {}
 
-//Listas
+  //Listas
 
-var lista1=["mazda", "chevrolet", "kia", 8];  
-List <string> lista2 ["mazda", "chevrolet", "kia"];  
-print(lista1.reversed);
-print(lista1.first);
-print(lista1.last);
-print(lista2);
+  var lista1=["mazda", "chevrolet", "kia", 8];  
+  List <String> lista2 = ["mazda", "chevrolet", "kia"];  
+  print(lista1.reversed);
+  print(lista1.first);
+  print(lista1.last);
+  print(lista2);
 
-lista1.add("toyota");
-lista1.clear();
-print(lista1);
+  lista1.add("toyota");
+  lista1.clear();
+  print(lista1);
 
-//lista null
-var lista4;
+  //lista null
+  var lista4;
 
-//Unir listas diferentes mediantre 3puntoseguidos y se incluye el ? para evitar crash con null values
-var lista3=["toyota", ...?lista1, ...?lista2, ...?lista4];
-//var lista3=["toyota"];
+  //Unir listas diferentes mediantre 3puntoseguidos y se incluye el ? para evitar crash con null values
+  var lista3=["toyota", ...?lista1, ...?lista2, ...?lista4];
+  //var lista3=["toyota"];
 
-print(lista3);
+  print(lista3);
 
 
 
-//Sets... 4 collections w {}
+  //Sets... 4 collections w {}
 
-var set1 = {'mazda', 'chevrolet', 'toyota', 'kia'};
-set1.add("Massive");
-set1.add("Korn");
-set1.add('Nissan');
+  var set1 = {'mazda', 'chevrolet', 'toyota', 'kia'};
+  set1.add("Massive");
+  set1.add("Korn");
+  set1.add('Nissan');
 
-print(set1);
+  print(set1);
 
-//Maps Traslations
-String llave ="Black";
-var traslation = {'red':'rojo', 'blue':'azul', 'green':'verde'};
+  //Maps Traslations
+  String llave ="Black";
+  var traslation = {'red':'rojo', 'blue':'azul', 'green':'verde'};
+  traslation['gray'] = "gris";
 
   traslation[llave]="negro";
-  var traslation;
   traslation["black"]="negro-negro";
   print(traslation);
-
-  translation['gray'] = "gris";
-}
+  }
