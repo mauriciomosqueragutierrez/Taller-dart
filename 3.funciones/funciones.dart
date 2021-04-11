afb87f4acd86e8c13cc2d9cdc2328a26c12f4a27
mauriciomosqueragutierrez <mauricio.mosquera.gutierrez@gmail.com>
2021-04-10 21:20:49 -0500

    Ajuste resultados funcionales

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -1,28 +1,24 @@
 void main() {
-  var resultado1 = obtenerNombre ("Mauricio");
-  var resultado2 = obtenerNombreDos ("Ricardo");
-  var resultado3 = obtenerNombreTres ("Oscar");
-  var resultado4 = obtenerNombreCuatro (valor: "Alejandro", edad: 15);
-
-  
+  var resultado1 = obtenerNombre("MAURICIO");
   print(resultado1);
+  var resultado2 = obtenerNombreDos("ALEJANDRO");
   print(resultado2);
+  var resultado3 = obtenerNombreTres(nombre: "RICARDO", edad: 30);
   print(resultado3);
+  var resultado4 = obtenerNombreCuatro(nombre: "SAMUEL", edad: 30);
   print(resultado4);
-
-
-  String obtenerNombreCuatro(String valor) {
-  return 'Mi nombre es $valor';
-  }
-
-  String obtenerNombre(String valor, [int? estatura, String? apellido]) {
-  return 'Mi nombre es $valor';
+  var resultado5 = obtenerNombreEspecial("MAURO", 30, "MOSQUERA");
+  print(resultado5);
 }
 
-  String obtenerNombreDos(String valor) =>'Mi nombre es $valor';
-
-  String obtenerNombreTres({String valor = 'Mateo', int? edad}) {
- return 'Mi nombre es $valor y mi edad es $edad';
-  }
-
+String obtenerNombre(String valor) {
+  return "Mi nombre es $valor";
 }
+
+String obtenerNombreDos(String valor) => "Mi nombre es $valor";
+
+String obtenerNombreTres({String nombre = "RICARDO", int? edad}) {return "Mi nombre es $nombre y mi edad es $edad";}
+
+String obtenerNombreCuatro({String nombre = "SAMUEL", required int? edad}) {return "Mi nombre es $nombre y mi edad es $edad";}
+
+String obtenerNombreEspecial(String nombre, [int? edad, String? apellido]) {return "Mi nombre es $nombre $apellido y mi edad es $edad";}
